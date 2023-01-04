f3e021f0e16a4fa5f6a2c9c7023a2a7f008dfac5
Bustamor <randall.bustamante@gmail.com>
2023-01-03 23:53:31 -0800

    practicing_js_fund

---
+++ React_real_estate/index.js
@@ -0,0 +1,3 @@
+let secletedColors = ['red', 'blue'];
+selectedColors[2] = 1;
+console.log (selectedColors.length);
---
+++ practicingjs/index.js
@@ -0,0 +1,13 @@
+
+function greet(name, lastName) {
+  console.log('Hello ' + name + ' ' + lastName);
+
+}
+
+//Calculate Value
+function square(number) {
+  return number * number;
+}
+
+
+console.log(square(4));
