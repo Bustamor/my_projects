2e07ae94ba71f2f0a3c64c6dc5034adcd226841c
Bustamor <randall.bustamante@gmail.com>
2023-01-08 00:20:01 -0800

    troubleshooting

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -6,14 +6,14 @@ import OperationButton from "./OperationButton"
 export const ACTIONS = {
   ADD_DIGIT: "add-digit",
   CHOOSE_OPERATION: "choose-operation",
-  CLEAR:'clear',
+  CLEAR: 'clear',
   DELETE_DIGIT: 'delete-digit',
   EVALUATE: 'evaluate',
 }
 
 
 function reducer(state, { type, payload }) {
-  switch(type) {
+  switch (type) {
     case ACTIONS.ADD_DIGIT:
       return {
         ...state,
@@ -23,37 +23,38 @@ function reducer(state, { type, payload }) {
 }
 
 
-  function App() {
-    const [{ currentOperand, previousOperand, operation}, dispatch] = useReducer(
-      reducer, 
-      {}
-    )
-    return (
-      <div className="calculator-grid">
-        <div className="output">
-          <div className="previous-operand">{previousOperand} {operation}</div>
-          <div className="current-operand">{currentOperand}</div>
-        </div>
-        <button className="span-two">AC</button>
-        <button>DEL</button>
-        <OperationButton operation="÷" dispatch={dispatch} />
-        <DigitButton digit="1" dispatch={dispatch} />
-        <DigitButton digit="2" dispatch={dispatch} />
-        <DigitButton digit="3" dispatch={dispatch} />
-        <OperationButton operation="*" dispatch={dispatch} />
-        <DigitButton digit="4" dispatch={dispatch} />
-        <DigitButton digit="5" dispatch={dispatch} />
-        <DigitButton digit="6" dispatch={dispatch} />
-        <OperationButton operation="+" dispatch={dispatch} />
-        <DigitButton digit="7" dispatch={dispatch} />
-        <DigitButton digit="8" dispatch={dispatch} />
-        <DigitButton digit="9" dispatch={dispatch} />
-        <OperationButton operation="-" dispatch={dispatch} />
-        <DigitButton digit="." dispatch={dispatch} />
-        <DigitButton digit="0" dispatch={dispatch} />
-        <button className="span-two">=</button>
+function App() {
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
+    reducer,
+    {}
+  )
+  
+  return (
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand">{previousOperand} {operation}</div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
-    )
-  }
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <OperationButton operation="÷" dispatch={dispatch} />
+      <DigitButton digit="1" dispatch={dispatch} />
+      <DigitButton digit="2" dispatch={dispatch} />
+      <DigitButton digit="3" dispatch={dispatch} />
+      <OperationButton operation="*" dispatch={dispatch} />
+      <DigitButton digit="4" dispatch={dispatch} />
+      <DigitButton digit="5" dispatch={dispatch} />
+      <DigitButton digit="6" dispatch={dispatch} />
+      <OperationButton operation="+" dispatch={dispatch} />
+      <DigitButton digit="7" dispatch={dispatch} />
+      <DigitButton digit="8" dispatch={dispatch} />
+      <DigitButton digit="9" dispatch={dispatch} />
+      <OperationButton operation="-" dispatch={dispatch} />
+      <DigitButton digit="." dispatch={dispatch} />
+      <DigitButton digit="0" dispatch={dispatch} />
+      <button className="span-two">=</button>
+    </div>
+  )
+}
 
 export default App;
