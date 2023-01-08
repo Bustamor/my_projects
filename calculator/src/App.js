49099a251e68bc8f7dda5df43a266e7e68079aca
Bustamor <randall.bustamante@gmail.com>
2023-01-07 23:49:17 -0800

    update

---
+++ calculator/src/App.js
@@ -0,0 +1,59 @@
+import "./styles.css"
+import { useReducer } from "react"
+import DigitButton from "./DigitButton"
+import OperationButton from "./OperationButton"
+
+export const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR:'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate',
+}
+
+
+function reducer(state, { type, payload }) {
+  switch(type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand: `${currentOperand || ""}${payload.digit}`,
+      }
+  }
+}
+
+
+  function App() {
+    const [{ currentOperand, previousOperand, operation}, dispatch] = useReducer(
+      reducer, 
+      {}
+    )
+    return (
+      <div className="calculator-grid">
+        <div className="output">
+          <div className="previous-operand">{previousOperand} {operation}</div>
+          <div className="current-operand">{currentOperand}</div>
+        </div>
+        <button className="span-two">AC</button>
+        <button>DEL</button>
+        <OperationButton operation="÷" dispatch={dispatch} />
+        <DigitButton digit="1" dispatch={dispatch} />
+        <DigitButton digit="2" dispatch={dispatch} />
+        <DigitButton digit="3" dispatch={dispatch} />
+        <OperationButton operation="*" dispatch={dispatch} />
+        <DigitButton digit="4" dispatch={dispatch} />
+        <DigitButton digit="5" dispatch={dispatch} />
+        <DigitButton digit="6" dispatch={dispatch} />
+        <OperationButton operation="+" dispatch={dispatch} />
+        <DigitButton digit="7" dispatch={dispatch} />
+        <DigitButton digit="8" dispatch={dispatch} />
+        <DigitButton digit="9" dispatch={dispatch} />
+        <OperationButton operation="-" dispatch={dispatch} />
+        <DigitButton digit="." dispatch={dispatch} />
+        <DigitButton digit="0" dispatch={dispatch} />
+        <button className="span-two">=</button>
+      </div>
+    )
+  }
+
+export default App;
