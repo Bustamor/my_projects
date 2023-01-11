e34a597dac3018aa9bbfd67d6ddbf111a2d43550
Bustamor <randall.bustamante@gmail.com>
2023-01-10 23:56:38 -0800

    init

--- calculator/src/OperationButton.js
+++ calculator/src/OperationButton.js
@@ -5,8 +5,7 @@ export default function OperationButton ({ dispatch, operation}) {
   return ( 
     <button 
       onClick={() => 
-        dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation }  })
-      }
+        dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation }  })}
     >
       {operation}
     </button>
