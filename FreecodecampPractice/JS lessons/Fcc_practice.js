a3b2d72058c2f9415c4fad804978661983ad9902
Bustamor <107319576+Bustamor@users.noreply.github.com>
2023-01-26 22:37:08 -0800

    JS practice init

---
+++ FreecodecampPractice/JS lessons/Fcc_practice.js
@@ -0,0 +1,22 @@
+// // .push
+// // Setup
+// const myArray = [["John", 23], ["cat", 2]];
+
+// // Only change code below this line
+// myArray.push(["dog", 3]);
+
+// // const threeArr = [1, 4, 6];
+// const oneDown = threeArr.pop();
+// console.log(oneDown);
+// console.log(threeArr);
+// The first console.log will display the value 6, and the second will display the value [1, 4].
+
+// Use the .pop() function to remove the last item from myArray and assign the popped off value to a new variable, removedFromMyArray.
+// Setup
+// const myArray = [["John", 23], ["cat", 2]];
+
+// // Only change code below this line
+// const removedFromMyArray = myArray.pop();
+
+// MANIPULATE ARRAYS WITH shift()
+
