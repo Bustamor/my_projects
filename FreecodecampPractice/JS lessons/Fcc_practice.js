9689c4fa06d309139380dd888077139d7f3fe431
Bustamor <107319576+Bustamor@users.noreply.github.com>
2023-01-30 00:30:04 -0800

    80%

--- FreecodecampPractice/JS lessons/Fcc_practice.js
+++ FreecodecampPractice/JS lessons/Fcc_practice.js
@@ -99,3 +99,228 @@
 //   // Only change code above this line
 
 // }
+
+// COMPARISONS WITH LOGICAL OPERATOR ||
+
+// function testLogicalOr(val) {
+//   // Only change code below this line
+
+//   if (val < 10 || val > 20) {
+//     return "Outside";
+//   }
+
+  
+
+//   // Only change code above this line
+//   return "Inside";
+// }
+
+// testLogicalOr(15);
+
+// IF ELSE STATEMENTS
+
+// function testElse(val) {
+//   let result = "";
+//   // Only change code below this line
+
+//   if (val > 5) {
+//     return   "Bigger than 5";
+//   } else  {
+//     return  "5 or Smaller";
+//   }
+
+//   // Only change code above this line
+//   return result;
+// }
+
+// testElse(4);
+
+// ELSE/IF
+// function testElseIf(val) {
+//   if (val > 10) {
+//     return "Greater than 10";
+//   }
+
+//    else if (val < 5) {
+//     return "Smaller than 5";
+//   }
+//   else {
+//   return "Between 5 and 10";
+//   }
+// }
+
+// testElseIf(7);
+
+// CHAINING ELSE/IF STATEMENTS
+// function testSize(num) {
+//   // Only change code below this line
+//   if (num < 5)
+//     return "Tiny"
+//     else if (num <10) {
+//     return "Small"
+//     }
+//     else if (num <15) {
+//     return "Medium"
+//     }
+//     else if (num < 20){
+//     return "Large"
+//     }
+//     else {
+//       return "Huge"
+//     }
+
+//   return "Change Me";
+//   // Only change code above this line
+// }
+
+// testSize(7);
+
+// GOLF CODE ELSE IF or TERNARY works here ***
+
+
+// const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+// function golfScore(par, strokes) {
+//   // Only change code below this line
+//     if (strokes ==1) {
+//       return names[0];
+//     }
+//     else if (strokes <= par-2) {
+//       return names[1];
+//     }
+//     else if (strokes == par-1) {
+//       return names[2];
+//     }
+//     else if (strokes == par) {
+//       return names[3];
+//     }
+//     else if (strokes == par +1){
+//       return names[4];
+//     } 
+//     else if (strokes == par +2) {
+//       return names[5];
+//     }
+    
+//   return names[6];
+//   // Only change code above this line
+// }
+
+// golfScore(5, 4);
+// SELECTING FROM MANY OPTIONS WITH SWITCH STATEMENTS
+
+// function caseInSwitch(val) {
+//   let answer = "";
+//   // Only change code below this line
+// switch (val){
+//   case 1:
+//   answer ="alpha";
+//   break;
+// }
+
+// switch (val){
+//   case 2:
+//   answer ="beta";
+//   break;
+// }
+// switch (val){
+//   case 3:
+//   answer ="gamma";
+//   break;
+// }
+// switch (val){
+//   case 4:
+//   answer ="delta"
+// }
+
+//   // Only change code above this line
+//   return answer;
+// }
+
+// caseInSwitch(1);
+
+// Adding a Default Option in Switch Statements
+
+// function switchOfStuff(val) {
+//   let answer = "";
+//   // Only change code below this line
+// switch(val) {
+//   case "a":
+//   answer = "apple"
+//   break;
+
+//   case "b":
+//   answer = "bird"
+//   break;
+
+//   case "c":
+//   answer = "cat"
+//   break;
+
+//   default:
+//   answer = "stuff";
+// }
+
+   
+
+//   // Only change code above this line
+//   return answer;
+// }
+
+// switchOfStuff(1);
+
+// CASE RANGES
+
+// COUNTING CARDS LOGIC
+
+// let count = 0;
+
+// function cc(card) {
+//   // Only change code below this line
+// switch (card) {
+//   case 2:
+//   case 3:
+//   case 4:
+//   case 5:
+//   case 6:
+//     count ++;
+//   break;
+//   case 10:
+//   case 'J':
+//   case 'Q':
+//   case 'K':
+//   case 'A':
+//     count --;
+//   break;
+// }
+// if (count > 0){
+//   return count + " Bet";
+// } else {
+//   return count + " Hold"
+// }
+  
+//   // Only change code above this line
+// }
+
+// cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+// **** USING OBJECTS FOR LOOKUPS
+// Setup
+// function phoneticLookup(val) {
+//   let result = "";
+
+//   // Only change code below this line
+//   var lookup = {
+//     "alpha": "Adams",
+//     "bravo": "Boston",
+//     "charlie":"Chicago",
+//     "delta": "Denver",
+//     "echo": "Easy",
+//     "foxtrot": "Frank"
+//   };
+
+// result = lookup[val]
+//   // Only change code above this line
+//   return result;
+// }
+
+// phoneticLookup("charlie");
