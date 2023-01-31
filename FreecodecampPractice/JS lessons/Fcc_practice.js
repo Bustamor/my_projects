d03a2b59ac8feac4a91e0accb244f6be6d7bff93
Bustamor <107319576+Bustamor@users.noreply.github.com>
2023-01-30 21:59:38 -0800

    82% record collection!

--- FreecodecampPractice/JS lessons/Fcc_practice.js
+++ FreecodecampPractice/JS lessons/Fcc_practice.js
@@ -324,3 +324,99 @@
 // }
 
 // phoneticLookup("charlie");
+
+// HAS OWN PROPERTY WITH BRACES (WHY)
+// function checkObj(obj, checkProp) {
+//   // Only change code below this line
+//    if (obj.hasOwnProperty(checkProp)){
+//        return obj[checkProp];
+
+//    }
+//   else {
+//       return "Not Found";
+  
+//   }
+//   // Only change code above this line
+// }
+
+// MANIPULATING COMPLEX OBJECTS 82%
+// const myMusic = [
+//   {
+//     "artist": "Paul Simon",
+//     "title": "Graceland",
+//     "release_year": 1986,
+//     "formats": [
+//       "CD",
+//       "LP",
+//       "8T"
+//     ],
+//   },
+//   {
+//     "artist": "Billy Joel",
+//     "title": "Piano Man",
+//     "release_year": 1973,
+//     "formats": [
+//       "CD",
+//       "8T",
+//       "LP"
+//     ],
+//     "gold": true
+//   }
+  
+// ];
+
+//  SUB PROPERTIES OF BRACKET AND DOT NOTATION
+// const myStorage = {
+//   "car": {
+//     "inside": {
+//       "glove box": "maps",
+//       "passenger seat": "crumbs"
+//      },
+//     "outside": {
+//       "trunk": "jack"
+//     }
+//   }
+// };
+
+
+// const gloveBoxContents = myStorage.car.inside["glove box"];
+
+// RECORD COLLECTION !!!*****
+// Setup
+// const recordCollection = {
+//   2548: {
+//     albumTitle: 'Slippery When Wet',
+//     artist: 'Bon Jovi',
+//     tracks: ['Let It Rock', 'You Give Love a Bad Name']
+//   },
+//   2468: {
+//     albumTitle: '1999',
+//     artist: 'Prince',
+//     tracks: ['1999', 'Little Red Corvette']
+//   },
+//   1245: {
+//     artist: 'Robert Palmer',
+//     tracks: []
+//   },
+//   5439: {
+//     albumTitle: 'ABBA Gold'
+//   }
+// };
+
+// // Only change code below this line
+// function updateRecords(records, id, prop, value) {
+
+//  if ( prop !== "tracks" && value !== ""  ) {
+//     records[id][prop] = value;
+//   } else if (prop === "tracks" && value !== "" && records[id].hasOwnProperty("tracks") === false) {
+  
+//    records[id][prop] = [value];
+//    } else if (prop === "tracks" && value != ""){
+//       records[id][prop].push(value);
+//     }
+//     else if (value == "") {
+//       delete records[id][prop];
+//     }
+//     return records;
+// }
+// updateRecords(recordCollection, 5439, 'artist', 'ABBA');
