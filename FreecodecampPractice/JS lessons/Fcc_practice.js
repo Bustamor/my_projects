5c5384e23839d2d0263b76cab7ebbc0aa18cb130
Bustamor <107319576+Bustamor@users.noreply.github.com>
2023-02-01 16:01:39 -0800

    92%

--- FreecodecampPractice/JS lessons/Fcc_practice.js
+++ FreecodecampPractice/JS lessons/Fcc_practice.js
@@ -420,3 +420,84 @@
 //     return records;
 // }
 // updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+
+// NESTING FOR LOOPS (ARRAYS IN ARRAYS)
+// function multiplyAll(arr) {
+//   let product = 1;
+//   // Only change code below this line
+// for (var i = 0; i < arr.length; i++) {
+//   for (var j = 0; j <arr[i].length; j++){
+//     product = product * arr[i][j];
+// }
+// }
+//   // Only change code above this line
+//   return product;
+// }
+
+// multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);  WHERE DOES (arr come from?)
+
+// RECURSION REPLACE LOOPS USING RECURSION
+// function sum(arr, n) {
+//   // Only change code below this line
+//  if (n<=0) {
+//    return 0;
+//  } else {
+//    return sum(arr, n-1) + arr [n -1];
+//  }
+ 
+//   // Only change code above this line
+// }
+
+// PROFILE LOOK UP HASOWNPROP and FOR LOOP and return
+// Setup
+// const contacts = [
+//   {
+//     firstName: "Akira",
+//     lastName: "Laine",
+//     number: "0543236543",
+//     likes: ["Pizza", "Coding", "Brownie Points"],
+//   },
+//   {
+//     firstName: "Harry",
+//     lastName: "Potter",
+//     number: "0994372684",
+//     likes: ["Hogwarts", "Magic", "Hagrid"],
+//   },
+//   {
+//     firstName: "Sherlock",
+//     lastName: "Holmes",
+//     number: "0487345643",
+//     likes: ["Intriguing Cases", "Violin"],
+//   },
+//   {
+//     firstName: "Kristian",
+//     lastName: "Vos",
+//     number: "unknown",
+//     likes: ["JavaScript", "Gaming", "Foxes"],
+//   },
+// ];
+
+// function lookUpProfile(name, prop) {
+//   // Only change code below this line
+// for (let i = 0; i < contacts.length; i++){
+//    if (contacts[i].firstName === name) {
+//       if (contacts[i].hasOwnProperty(prop)) {
+//         return contacts[i][prop];
+//       } else {
+//         return "No such property";
+//       }
+//     }
+//   }
+//   return "No such contact";
+// }
+
+// GENERATE RANDOME WHOLE NUMBERS W JSONfunction randomWholeNum() {
+
+//   // Only change code below this line
+
+// function randomWholeNum() {
+
+//   // Only change code below this line
+
+//   return Math.floor(Math.random() * 10);
+// }
