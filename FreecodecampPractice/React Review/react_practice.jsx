fec24ad15b39f87154c6856b32dd22f8eca9b216
Bustamor <107319576+Bustamor@users.noreply.github.com>
2023-02-02 23:55:31 -0800

    26%

--- FreecodecampPractice/React Review/react_practice.jsx
+++ FreecodecampPractice/React Review/react_practice.jsx
@@ -9,4 +9,22 @@
 // ReactDOM.render(JSX, document.getElementById("challenge-node")
 // );
 
-
+// Class Component RENDERED TO DOM
+// class TypesOfFood extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>Types of Food:</h1>
+//         {/* Change code below this line */}
+//         <Fruits />
+//         <Vegetables />
+//         {/* Change code above this line */}
+//       </div>
+//     );
+//   }
+// };
+// ReactDOM.render(<TypesOfFood/>, document.getElementById('challenge-node'))
+// // Change code below this line
